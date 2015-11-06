d4306108a6dd48a07b41bb514a3c71e17a9464f8
Solvie <lee.solvie@gmail.com>
2015-11-05 20:09:52 -0500

    Shawn's modifications integrated to ObjectDetector

--- Unit_Tests/Navigator_Unit/src/naviUnit/ObjectDetector.java
+++ Unit_Tests/Navigator_Unit/src/naviUnit/ObjectDetector.java
@@ -1,56 +1,97 @@
-package naviUnit;
+package robot;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 import lejos.robotics.SampleProvider;
-import lejos.utility.Timer;
+import lejos.utility.Delay;
 import lejos.utility.TimerListener;
+import lejos.utility.Timer;
+/*
+ * Here is the tiniest change I can think of. One I know this works, I will continue to use git properly!
+ */
+/**
+ * The object detector class is responsible for using the ultrasonic sensor and the light sensor to detect
+ * objects in front of the robot. 
+ * 
+ * @version 0.5
+ *
+ */
 
-public class ObjectDetector implements TimerListener{
+public class ObjectDetector implements TimerListener {
 	
-	public static final int errorMargin = 2, DELAY = 10, offFactor = 20;
-	private static final double OBJECT_CLOSE = 7, OBJECT_FAR = 15, MAX_RANGE = 120;
-	private boolean objectDetected, objectClose, objectColorSeen, flagDetected;
-	private int i =0;
-	private SampleProvider usValue, colorValue;
-	private int color, blockDetected;
-	private float[] usData, colorData;
-	private Timer timer;
+	private static final int DEFAULT_INTERVAL = 25, OBJECT_CLOSE = 15, OBJECT_FAR = 90;
 	private Navigator navi;
 	private Odometer odo;
-	private double distance;
+	private Timer timer;
+	private boolean objectDetected, objectClose, colorSeen, flagDetected;
+	private SampleProvider usSensor, colorSensor;
+	private int distance, filter;
+	private int filter_out=10;
+	private float[] usData, colorData;
+	private ArrayList<Integer> Data=new ArrayList<Integer>();
 	
-	public ObjectDetector(Navigator navi, SampleProvider usValue, float[] usData,
-			SampleProvider colorValue, float[] colorData, boolean autostart) {
-		this.objectDetected = false;
-		this.blockDetected = -1;
-		this.distance = MAX_RANGE; // initialize distance to farthest.
-		this.usValue = usValue;
-		this.colorValue = colorValue;
-		this.usData = usData;
-		this.colorData = colorData;
+	/**
+	 * Default constructor
+	 * @param navi the navigator
+	 * @param usSensor the SampleProvider for ultrasonic values
+	 * @param usValue the ultrasonic values from the SampleProvider
+	 * @param colorSensor the SampleProvider for color values
+	 * @param colorValue the color values from the SampleProvider.
+	 */
+	public ObjectDetector(Navigator navi, SampleProvider usSensor, float[] usData, SampleProvider colorSensor, float[] colorData, boolean AUTOSTART){
 		this.navi = navi;
-		this.odo = null;
-		this.timer = new Timer(DELAY, this);
-		if (autostart)
-			timer.start();
+		this.odo = navi.getOdo();
+		this.distance=getrawdistance();
+		this.timer = new Timer(DEFAULT_INTERVAL, this);
+		if (AUTOSTART)
+			this.timer.start();
 	}
 	
+	/**
+	 * This method checks the sensor data whenever it is called. It also tells the navigator whether an object/flag was seen or not. 
+	 */
 	public void timedOut(){
-		//Check sensor data every time 	the timer times out
-		usValue.fetchSample(usData,0);
-		distance = (usData[0] * 100);
-		//distance = filter(distance);
-		colorValue.fetchSample(colorData, 0);
-		//color = something.
+		synchronized (this){
+			//Check sensor data every time 	the timer times out
+			distance = filter(distance);
+			
+		}
 		//Tell the navi the information.
 		updateNavi(OBJECT_FAR, OBJECT_CLOSE);
-		}
+		
+	}
 	
-	public double filter(double distance){
-		//placeholder filtering method.
+	/**
+	 * This is a temporary placeholder method to filter the distance data. 
+	 * @param distance data fetched from the ultrasonic. 
+	 */
+	private int filter(int distance){
+		int lastdis=distance;
+		Data.clear();
+		for(int i=0;i<46;i++){
+			Data.add(this.getrawdistance());
+			Delay.usDelay(500);
+		}
+		Collections.sort(Data);
+		int currentdis=Data.get(Data.size()/2);
+		int diff=Math.abs(currentdis-lastdis);
+		if(diff>20 && filter<filter_out){
+			filter++;
+		}else{
+			distance=currentdis;
+			filter=0;
+		}
 		return distance;
 	}
 	
-	public void updateNavi(double farDist, double closeDist){
+	/**
+	 * This method allows the object detector to tell the navigator the state of the field of vision.
+	 * 
+	 * @param farDist Distance within which the object detector will say an object has been detected
+	 * @param closeDist Distance within which the object detector will say an object is close
+	 */
+	public void updateNavi(int farDist, int closeDist){
 		//notify navi if object 
 		if (distance <= farDist)
 			objectDetected = true;
@@ -65,31 +106,45 @@ public class ObjectDetector implements TimerListener{
 		navi.setDetectionInfo(new boolean[]{objectDetected, objectClose, false},new boolean[]{true, true, false});
 	}
 	
-	public boolean[] getVisionStatus(){
-		boolean[] status = new boolean[4];
+	/**
+	 * This method allows outside classes to get all the information from the objectDetector, whether an object was seen, whether its color was seen, etc.
+	 * @param status the array of boolean values to be filled with the information requested. 
+	 */
+	public boolean[] getVisionStatus(boolean[] status){
 		status[0] = objectDetected;
-		status[1] = objectClose;
-		status[2] = objectColorSeen;
-		status[3] = flagDetected;
+		status[1] = colorSeen;
+		status[2] = flagDetected;
 		return status;
 	}
 	
-	public int[] getDistanceAndColor(){
-		int[] distanceNColor = new int[2];
-		distanceNColor[0] = (int) distance;
-		distanceNColor[1] = color;
-		return distanceNColor;
-	}
-	
+	/**
+	 * This method allows outside classes to access the ObjectDetector's navigator.
+	 * @return Navigator
+	 */
 	public Navigator getNavi(){
 		return navi;
 	}
 	
-	public void start(){
-		timer.start();
+	public int getfilterdistance(){
+		return distance;
 	}
-
-	public void stop(){
-		timer.stop();
+	
+	public int getrawdistance(){
+		synchronized (this){
+			usSensor.fetchSample(usData, 0);
+			int rawdistance = (int)(usData[0]*100.0);
+			// filter infinity
+			if(rawdistance==Integer.MAX_VALUE){
+				rawdistance=255;}
+			return rawdistance;
+		}
+	}
+	
+	private int getcolor(){
+		synchronized (this){
+			colorSensor.fetchSample(colorData, 0);
+			int color=(int)(colorData[0]*100);		
+			return color;
+		}
 	}
 }
